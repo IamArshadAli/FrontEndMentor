60c32471dadca5d79491edff9beb3ce52a7526a8
IamArshadAli <arshadalikaldane@gmail.com>
2023-10-31 21:35:56 +0530

    utils 1-1

---
+++ src/utils/1-newbie/1-results-summary/data.js
@@ -0,0 +1,37 @@
+import {
+  memory,
+  reaction,
+  verbal,
+  visual,
+} from "../../../assets/images/1-newbie/1-results-summary";
+
+export const data = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: reaction,
+    color: "text-LightRed",
+    background: "bg-LightRed",
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: memory,
+    color: "text-OrangeYellow",
+    background: "bg-OrangeYellow",
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: verbal,
+    color: "text-GreenTeal",
+    background: "bg-GreenTeal",
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: visual,
+    color: "text-CobaltBlue",
+    background: "bg-CobaltBlue",
+  },
+];
