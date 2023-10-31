046d8a8c309ed30fbbf655e4f8d728e1d2478520
IamArshadAli <arshadalikaldane@gmail.com>
2023-10-31 21:34:16 +0530

    custom styles 1-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // ! 1-1
+        HankenGrotesk: ['Hanken Grotesk', 'sans-serif'],
+      },
+      colors: {
+        // ! 1-1
+        // Primary
+        LightRed: "hsl(0, 100%, 67%)",
+        OrangeYellow: "hsl(39, 100%, 56%)",
+        GreenTeal: "hsl(166, 100%, 37%)",
+        CobaltBlue: "hsl(234, 85%, 45%)",
+        // Neutral
+        White: "hsl(0, 0%, 100%)",
+        PaleBlue: "hsl(221, 100%, 96%)",
+        LightLavender: "hsl(241, 100%, 89%)",
+        DarkGrayBlue: "hsl(224, 30%, 27%)",
+        // Grad I
+        SlateBlue: "hsl(252, 100%, 67%)",
+        RoyalBlue: "hsl(241, 81%, 54%)",
+        // Grad II
+        VioletBlue: "hsla(256, 72%, 46%, 1)",
+        PersianBlue: "hsla(241, 72%, 46%, 0)",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: ['prettier-plugin-tailwindcss'],
+};
