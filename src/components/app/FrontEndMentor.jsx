9c1f6e27b4f1ef750d67c2cfd6d2885fe82e3077
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-01 17:12:05 +0530

    layout

---
+++ src/components/app/FrontEndMentor.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/prop-types */
+import { Link, NavLink } from "react-router-dom";
+import { AppData } from "../../utils/app/data";
+
+const Cards = () => {
+  const Chip = ({ style, name }) => {
+    return (
+      <div
+        className={`${style} bg-opacity-10 px-3 py-1 rounded font-semibold uppercase text-xs`}
+      >
+        {name}
+      </div>
+    );
+  };
+  return (
+    <>
+      {AppData.map((data, index) => (
+        <div
+          key={index}
+          className="max-w-sm overflow-hidden shadow-lg rounded-xl bg-neutral-50"
+        >
+          {/* image */}
+          <Link to={data.link}>
+            <img src={data.image} alt={data.name} />
+          </Link>
+          {/* name */}
+          <div className="p-4 text-DarkGrayishBlue">
+            <NavLink
+              to={data.link}
+              className="hover:underline hover:underline-offset-2 text-lg font-HankenGrotesk font-semibold"
+            >
+              {data.name}
+            </NavLink>
+            {/* tags | level */}
+            <div className="flex justify-between pt-3">
+              <div className="flex space-x-3">
+                {data.tags.map((tag, index) => (
+                  <Chip key={index} style={tag.style} name={tag.name} />
+                ))}
+              </div>
+              <Chip style={data.level.style} name={data.level.name} />
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+const FrontEndMentor = () => {
+  return (
+    <main className="w-screen min-h-screen bg-PaleBlue">
+      <div className="container max-w-7xl mx-auto">
+        <header className="text-center">
+          <div className="text-3xl text-DarkGrayishBlue font-bold tracking-wide font-Fraunces py-4">
+            FrontEndMentor Designs
+          </div>
+        </header>
+        <section className="py-4 px-6 flex flex-col md:flex-row gap-6 md:gap-12">
+          <Cards />
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default FrontEndMentor;
