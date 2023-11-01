0e4ff28e538f7d1d89f34ed4c6550c18c593864f
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-01 13:15:59 +0530

    assets 1-2

---
+++ src/assets/images/1-newbie/2-product-card/index.js
@@ -0,0 +1,5 @@
+import cartIcon from "./icon-cart.svg";
+import productPC from "./image-product-desktop.jpg";
+import productMobile from "./image-product-mobile.jpg";
+
+export { cartIcon, productPC, productMobile };
