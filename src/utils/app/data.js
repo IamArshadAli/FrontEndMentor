9bbb48ca241624d947427b7b7053d55d1fd2d03e
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-02 13:03:05 +0530

    data 1-3

--- src/utils/app/data.js
+++ src/utils/app/data.js
@@ -1,4 +1,4 @@
-import { N1, N2 } from "../../assets/images/app";
+import { N1, N2, N3 } from "../../assets/images/app";
 
 const LEVEL = {
   newbie: { name: "newbie", style: "text-DarkCyan border border-DarkCyan" },
@@ -7,25 +7,31 @@ const LEVEL = {
   advanced: { name: "advanced", style: "text- border border-" },
 };
 
+const TAGS = {
+  html: { name: "html", style: "text-CobaltBlue bg-CobaltBlue" },
+  css: { name: "css", style: "text-GreenTeal bg-GreenTeal" },
+};
+
 export const AppData = [
   {
     image: N1,
     name: "Result Summary",
-    tags: [
-      { name: "html", style: "text-CobaltBlue bg-CobaltBlue" },
-      { name: "css", style: "text-GreenTeal bg-GreenTeal" },
-    ],
+    tags: [TAGS.html, TAGS.css],
     level: LEVEL.newbie,
     link: `${LEVEL.newbie.name}/result-summary`,
   },
   {
     image: N2,
     name: "Product Preview Card",
-    tags: [
-      { name: "html", style: "text-CobaltBlue bg-CobaltBlue" },
-      { name: "css", style: "text-GreenTeal bg-GreenTeal" },
-    ],
+    tags: [TAGS.html, TAGS.css],
     level: LEVEL.newbie,
     link: `${LEVEL.newbie.name}/product-card`,
+  },
+  {
+    image: N3,
+    name: "NFT Preview Card",
+    tags: [TAGS.html, TAGS.css],
+    level: LEVEL.newbie,
+    link: `${LEVEL.newbie.name}/nft-card`,
   },
 ];
