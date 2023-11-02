ffbccade3b1d7b1eb8d51b21a294d333af70530d
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-02 13:01:57 +0530

    assets 1-3

---
+++ src/assets/images/1-newbie/3-nft-card/index.js
@@ -0,0 +1,7 @@
+import clockIcon from "./icon-clock.svg";
+import ethereumIcon from "./icon-ethereum.svg";
+import viewIcon from "./icon-view.svg";
+import avatarImg from "./image-avatar.png";
+import equilibriumImg from "./image-equilibrium.jpg";
+
+export { clockIcon, ethereumIcon, viewIcon, avatarImg, equilibriumImg };
