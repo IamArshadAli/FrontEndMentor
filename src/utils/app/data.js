f740a2e201fb3f524cfed45c8c4bcd5d457f236c
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-03 20:24:45 +0530

    data 1-4

--- src/utils/app/data.js
+++ src/utils/app/data.js
@@ -1,4 +1,4 @@
-import { N1, N2, N3 } from "../../assets/images/app";
+import { N1, N2, N3, N4 } from "../../assets/images/app";
 
 const LEVEL = {
   newbie: { name: "newbie", style: "text-DarkCyan border border-DarkCyan" },
@@ -10,6 +10,7 @@ const LEVEL = {
 const TAGS = {
   html: { name: "html", style: "text-CobaltBlue bg-CobaltBlue" },
   css: { name: "css", style: "text-GreenTeal bg-GreenTeal" },
+  js: { name: "js", style: "text-OrangeYellow bg-OrangeYellow" },
 };
 
 export const AppData = [
@@ -33,5 +34,12 @@ export const AppData = [
     tags: [TAGS.html, TAGS.css],
     level: LEVEL.newbie,
     link: `${LEVEL.newbie.name}/nft-card`,
+  },
+  {
+    image: N4,
+    name: "Interactive Rating",
+    tags: [TAGS.html, TAGS.css, TAGS.js],
+    level: LEVEL.newbie,
+    link: `${LEVEL.newbie.name}/interactive-rating`,
   },
 ];
