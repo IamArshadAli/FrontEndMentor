31b947657ab7bb66ca89bb7837283e062c37cce7
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-04 22:27:19 +0530

    new: introduced constants

---
+++ src/utils/app/constants.js
@@ -0,0 +1,24 @@
+const ROUTES = {
+  newbie: {
+    resultSummary: "result-summary",
+    productCard: "product-card",
+    nftCard: "nft-card",
+    interactiveRating: "interactive-rating",
+    statsCard: "stats-card",
+  },
+};
+
+const LEVEL = {
+  newbie: { name: "newbie", style: "text-DarkCyan border border-DarkCyan" },
+  junior: { name: "junior", style: "text- border border-" },
+  intermediate: { name: "intermediate", style: "text- border border-" },
+  advanced: { name: "advanced", style: "text- border border-" },
+};
+
+const TAGS = {
+  html: { name: "html", style: "text-CobaltBlue bg-CobaltBlue" },
+  css: { name: "css", style: "text-GreenTeal bg-GreenTeal" },
+  js: { name: "js", style: "text-OrangeYellow bg-OrangeYellow" },
+};
+
+export { ROUTES, LEVEL, TAGS };
