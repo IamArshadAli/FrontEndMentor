22f1615c82382f068a76ec96dccb93780c122af3
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-05 12:26:56 +0530

    image 1-6

--- src/assets/images/app/index.js
+++ src/assets/images/app/index.js
@@ -3,5 +3,6 @@ import N2 from "./1-2.jpg";
 import N3 from "./1-3.jpg";
 import N4 from "./1-4.jpg";
 import N5 from "./1-5.jpg";
+import N6 from "./1-6.jpg";
 
-export { N1, N2, N3, N4, N5 };
+export { N1, N2, N3, N4, N5, N6 };
