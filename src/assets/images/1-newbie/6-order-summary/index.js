508ba23eae67247dbcadee7004806067511fb530
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-05 12:26:35 +0530

    assets 1-6

---
+++ src/assets/images/1-newbie/6-order-summary/index.js
@@ -0,0 +1,6 @@
+import musicIcon from "./icon-music.svg";
+import heroIllustration from "./illustration-hero.svg";
+import patternBgMobile from "./pattern-background-mobile.svg";
+import patternBgPC from "./pattern-background-desktop.svg";
+
+export { musicIcon, patternBgMobile, patternBgPC, heroIllustration };
