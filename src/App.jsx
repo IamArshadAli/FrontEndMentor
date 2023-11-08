81be01fc5040d129f0a983fa982333fe70458ca3
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-08 10:48:35 +0530

    conditional basename config

--- src/App.jsx
+++ src/App.jsx
@@ -14,36 +14,37 @@ import {
 
 function App() {
   return (
-    <>
-      <BrowserRouter basename="FrontEndMentor">
-        <Routes>
-          <Route path="/FrontEndMentor" element={<FrontEndMentor />} />
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "FrontEndMentor"}>
+      <Routes>
+        <Route path="/FrontEndMentor" element={<FrontEndMentor />} />
+        <Route
+          path="FrontEndMentor/newbie"
+          element={
+            <>
+              <Outlet />
+            </>
+          }
+        >
           <Route
-            path="FrontEndMentor/newbie"
-            element={
-              <>
-                <Outlet />
-              </>
-            }
-          >
-            <Route
-              path={ROUTES.newbie.resultSummary}
-              element={<ResultSummary />}
-            />
-            <Route path={ROUTES.newbie.productCard} element={<ProductCard />} />
-            <Route path={ROUTES.newbie.nftCard} element={<NFTCard />} />
-            <Route
-              path={ROUTES.newbie.interactiveRating}
-              element={<InteractiveRating />}
-            />
-            <Route path={ROUTES.newbie.statsCard} element={<StatsCard />} />
-            <Route path={ROUTES.newbie.orderSummary} element={<OrderSummary />} />
-            <Route path={ROUTES.newbie.qrCode} element={<QRCode />} />
-            <Route path={ROUTES.newbie.threeColumnCard} element={<ThreeColumnCard />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+            path={ROUTES.newbie.resultSummary}
+            element={<ResultSummary />}
+          />
+          <Route path={ROUTES.newbie.productCard} element={<ProductCard />} />
+          <Route path={ROUTES.newbie.nftCard} element={<NFTCard />} />
+          <Route
+            path={ROUTES.newbie.interactiveRating}
+            element={<InteractiveRating />}
+          />
+          <Route path={ROUTES.newbie.statsCard} element={<StatsCard />} />
+          <Route path={ROUTES.newbie.orderSummary} element={<OrderSummary />} />
+          <Route path={ROUTES.newbie.qrCode} element={<QRCode />} />
+          <Route
+            path={ROUTES.newbie.threeColumnCard}
+            element={<ThreeColumnCard />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
