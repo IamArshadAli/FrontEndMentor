ac349013f0f37b26aef3b5e87cae73c0ecc886ed
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-08 12:06:27 +0530

    add: basename

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/FrontEndMentor"}>
       <Routes>
         <Route path="/FrontEndMentor" element={<FrontEndMentor />} />
         <Route
