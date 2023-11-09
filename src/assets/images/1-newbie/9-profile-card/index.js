3b9e989e72731eb490b3d07516e7729cf709b0ef
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-09 15:20:18 +0530

    assets

---
+++ src/assets/images/1-newbie/9-profile-card/index.js
@@ -0,0 +1,6 @@
+import patternBottomBG from "./bg-pattern-bottom.svg";
+import patternTopB from "./bg-pattern-top.svg";
+import patternCardBG from "./bg-pattern-card.svg";
+import victorImg from "./image-victor.jpg";
+
+export { patternBottomBG, patternTopB, patternCardBG, victorImg };
