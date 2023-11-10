93a1d2b580b9a74e2197fa3b3b7d403c9b78b781
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-10 22:07:34 +0530

    data 1-10

--- src/utils/app/data.js
+++ src/utils/app/data.js
@@ -1,4 +1,4 @@
-import { N1, N2, N3, N4, N5, N6, N7, N8, N9 } from "../../assets/images/app";
+import { N1, N2, N3, N4, N5, N6, N7, N8, N9, N10 } from "../../assets/images/app";
 
 import { LEVEL, TAGS, ROUTES } from "./constants";
 
@@ -65,5 +65,12 @@ export const AppData = [
     tags: [TAGS.html, TAGS.css],
     level: LEVEL.newbie,
     link: `${LEVEL.newbie.name}/${ROUTES.newbie.profileCard}`,
+  },
+  {
+    image: N10,
+    name: "FAQs Accordion Card",
+    tags: [TAGS.html, TAGS.css, TAGS.js],
+    level: LEVEL.newbie,
+    link: `${LEVEL.newbie.name}/${ROUTES.newbie.faqAccordion}`,
   },
 ];
