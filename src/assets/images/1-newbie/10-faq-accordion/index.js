09bfef6b610969bc0ded84386515c7bb3521046e
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-10 22:06:59 +0530

    assets 1-10

---
+++ src/assets/images/1-newbie/10-faq-accordion/index.js
@@ -0,0 +1,8 @@
+import bgPatternPC from "./bg-pattern-desktop.svg";
+import bgPatternMobile from "./bg-pattern-mobile.svg";
+import womanOnlineIllustrationPC from "./illustration-woman-online-desktop.svg";
+import womanOnlineIllustrationMobile from "./illustration-woman-online-mobile.svg";
+import boxIllustration from "./illustration-box-desktop.svg";
+import arrowDownIcon from "./icon-arrow-down.svg";
+
+export {bgPatternPC, bgPatternMobile, womanOnlineIllustrationPC, womanOnlineIllustrationMobile, boxIllustration, arrowDownIcon}
