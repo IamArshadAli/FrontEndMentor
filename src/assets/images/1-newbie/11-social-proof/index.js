b1deb1e47c5993ec4f10e29fdec437e63da5e566
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-11 21:33:16 +0530

    assets: 1-11

---
+++ src/assets/images/1-newbie/11-social-proof/index.js
@@ -0,0 +1,19 @@
+import bgPatternBottomPC from "./bg-pattern-bottom-desktop.svg";
+import bgPatternBottomMobile from "./bg-pattern-bottom-mobile.svg";
+import bgPatternTopPC from "./bg-pattern-top-desktop.svg";
+import bgPatternTopMobile from "./bg-pattern-top-mobile.svg";
+import starIcon from "./icon-star.svg";
+import anneImage from "./image-anne.jpg";
+import coltonImage from "./image-colton.jpg";
+import ireneImage from "./image-irene.jpg";
+
+export {
+  bgPatternBottomPC,
+  bgPatternBottomMobile,
+  bgPatternTopPC,
+  bgPatternTopMobile,
+  starIcon,
+  anneImage,
+  coltonImage,
+  ireneImage,
+};
