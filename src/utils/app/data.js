d9eabea09088b663b8451ece8d12efa35bbc6628
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-11 21:32:23 +0530

    data:  1-11

--- src/utils/app/data.js
+++ src/utils/app/data.js
@@ -1,4 +1,4 @@
-import { N1, N2, N3, N4, N5, N6, N7, N8, N9, N10 } from "../../assets/images/app";
+import { N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11 } from "../../assets/images/app";
 
 import { LEVEL, TAGS, ROUTES } from "./constants";
 
@@ -72,5 +72,12 @@ export const AppData = [
     tags: [TAGS.html, TAGS.css, TAGS.js],
     level: LEVEL.newbie,
     link: `${LEVEL.newbie.name}/${ROUTES.newbie.faqAccordion}`,
+  },
+  {
+    image: N11,
+    name: "Social Proof Section",
+    tags: [TAGS.html, TAGS.css],
+    level: LEVEL.newbie,
+    link: `${LEVEL.newbie.name}/${ROUTES.newbie.socialProofSection}`,
   },
 ];
