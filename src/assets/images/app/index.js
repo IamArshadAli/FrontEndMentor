728e1b169f81815664013b691195ae346d37bbb5
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-11 21:32:51 +0530

    image: 1-11

--- src/assets/images/app/index.js
+++ src/assets/images/app/index.js
@@ -8,5 +8,6 @@ import N7 from "./1-7.jpg";
 import N8 from "./1-8.jpg";
 import N9 from "./1-9.jpg";
 import N10 from "./1-10.jpg";
+import N11 from "./1-11.jpg";
 
-export { N1, N2, N3, N4, N5, N6, N7, N8, N9, N10 };
+export { N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11 };
