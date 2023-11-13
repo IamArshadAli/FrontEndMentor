874c4b7189e156c5272ebde207f50b403493d41a
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-13 13:20:57 +0530

    assets 1-13

---
+++ src/assets/images/1-newbie/13-four-card-feature/index.js
@@ -0,0 +1,6 @@
+import calculatorIcon from "./icon-calculator.svg";
+import karmaIcon from "./icon-karma.svg";
+import supervisorIcon from "./icon-supervisor.svg";
+import teamBuilderIcon from "./icon-team-builder.svg";
+
+export { calculatorIcon, karmaIcon, supervisorIcon, teamBuilderIcon };
