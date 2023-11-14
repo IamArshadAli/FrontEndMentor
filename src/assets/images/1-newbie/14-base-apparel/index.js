e5b28a0cead4aa40b226c4bd66aeac295ad24316
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-14 18:06:25 +0530

    assets 1-14

---
+++ src/assets/images/1-newbie/14-base-apparel/index.js
@@ -0,0 +1,8 @@
+import bgPatternPC from "./bg-pattern-desktop.svg";
+import heroPC from "./hero-desktop.jpg";
+import heroMobile from "./hero-mobile.jpg";
+import arrowIcon from "./icon-arrow.svg";
+import errorIcon from "./icon-error.svg";
+import logo from "./logo.svg";
+
+export { bgPatternPC, heroPC, heroMobile, arrowIcon, errorIcon, logo };
