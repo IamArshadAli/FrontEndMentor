a91c531619250726717bc0603ffffc71d88762b1
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-14 19:05:02 +0530

    assets 1-15

---
+++ src/assets/images/1-newbie/15-intro-component/index.js
@@ -0,0 +1,5 @@
+import bgIntroPC from "./bg-intro-desktop.png";
+import bgIntroMobile from "./bg-intro-mobile.png";
+import errorIcon from "./icon-error.svg";
+
+export { bgIntroPC, bgIntroMobile, errorIcon };
