992515f303fed93468d3b06f0058fae810f62a7c
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-16 22:53:35 +0530

    huddle landing page 1-18

---
+++ src/components/1-newbie/18-huddle-landing-page/HuddleLandingPage.jsx
@@ -0,0 +1,12 @@
+import {
+  bgPC,
+  bgMobile,
+  mockupIllustration,
+  logo,
+} from "../../../assets/images/1-newbie/18-huddle-landing-page";
+
+const HuddleLandingPage = () => {
+  return <div>HuddleLandingPage</div>;
+};
+
+export default HuddleLandingPage;
