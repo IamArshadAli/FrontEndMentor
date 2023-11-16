0a195bbc35286f104c92e371057a9d179c92a32c
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-16 22:52:36 +0530

    assets 1-18

---
+++ src/assets/images/1-newbie/18-huddle-landing-page/index.js
@@ -0,0 +1,6 @@
+import bgPC from "./bg-desktop.svg";
+import bgMobile from "./bg-mobile.svg";
+import mockupIllustration from "./illustration-mockups.svg";
+import logo from "./logo.svg";
+
+export { bgPC, bgMobile, mockupIllustration, logo };
