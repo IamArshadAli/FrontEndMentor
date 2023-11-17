8099910a52245024f8d6e21748c2ee3bfd78fb0a
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-17 22:18:18 +0530

    newsletter sign up 2-1

--- src/components/2-junior/1-newsletter-sign-up/NewsLetterSignUp.jsx
+++ src/components/2-junior/1-newsletter-sign-up/NewsLetterSignUp.jsx
@@ -1,12 +1,25 @@
-import {
-  listIcon,
-  successIcon,
-  signUpIllustrationPc,
-  signUpIllustrationMobile,
-} from "../../../assets/images/2-junior/1-newsletter-sign-up";
+import { useState } from "react";
+
+import SignUpForm from "./SignUpForm";
+import SignUpSuccess from "./SignUpSuccess";
 
 const NewsLetterSignUp = () => {
-  return <div>NewsLetterSignUp</div>;
+  const [email, setEmail] = useState("");
+  const [currentState, setCurrentState] = useState("signup");
+
+  return (
+    <main className="w-screen h-screen bg-CharcoalGrey text-DarkSlateGrey grid place-items-center font-Roboto">
+      {currentState === "success" ? (
+        <SignUpSuccess email={email} setEmail={setEmail} setCurrentState={setCurrentState} />
+      ) : (
+        <SignUpForm
+          email={email}
+          setEmail={setEmail}
+          setCurrentState={setCurrentState}
+        />
+      )}
+    </main>
+  );
 };
 
 export default NewsLetterSignUp;
