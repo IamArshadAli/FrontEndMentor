b1065c872def25e95454e40c6c814037f0eebff9
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-17 18:40:22 +0530

    news letter sign up 2-1

---
+++ src/components/2-junior/1-newsletter-sign-up/NewsLetterSignUp.jsx
@@ -0,0 +1,12 @@
+import {
+  listIcon,
+  successIcon,
+  signUpIllustrationPc,
+  signUpIllustrationMobile,
+} from "../../../assets/images/2-junior/1-newsletter-sign-up";
+
+const NewsLetterSignUp = () => {
+  return <div>NewsLetterSignUp</div>;
+};
+
+export default NewsLetterSignUp;
---
+++ src/components/2-junior/index.js
@@ -0,0 +1,3 @@
+import NewsLetterSignUp from "./1-newsletter-sign-up/NewsLetterSignUp";
+
+export { NewsLetterSignUp };
