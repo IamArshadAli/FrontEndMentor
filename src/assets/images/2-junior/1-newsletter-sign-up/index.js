9614671e45bde030c5369b5e7a02264fc04142bf
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-17 18:40:02 +0530

    assets 2-1

---
+++ src/assets/images/2-junior/1-newsletter-sign-up/index.js
@@ -0,0 +1,11 @@
+import listIcon from "./icon-list.svg";
+import successIcon from "./icon-success.svg";
+import signUpIllustrationPc from "./illustration-sign-up-desktop.svg";
+import signUpIllustrationMobile from "./illustration-sign-up-mobile.svg";
+
+export {
+  listIcon,
+  successIcon,
+  signUpIllustrationPc,
+  signUpIllustrationMobile,
+};
