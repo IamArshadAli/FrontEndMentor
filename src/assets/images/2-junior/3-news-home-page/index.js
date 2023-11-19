6f1377c4f3f21b3bafabb53b987e0d872e47b1ad
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-19 13:24:20 +0530

    assets 2-3

---
+++ src/assets/images/2-junior/3-news-home-page/index.js
@@ -0,0 +1,19 @@
+import menuIcon from "./icon-menu.svg";
+import menuCloseIcon from "./icon-menu-close.svg";
+import logo from "./logo.svg";
+import gamingGrowthImage from "./image-gaming-growth.jpg";
+import retroPcsImage from "./image-retro-pcs.jpg";
+import topLaptopsImage from "./image-top-laptops.jpg";
+import web3DesktopImage from "./image-web-3-desktop.jpg";
+import web3MobileImage from "./image-web-3-mobile.jpg";
+
+export {
+  menuIcon,
+  menuCloseIcon,
+  logo,
+  gamingGrowthImage,
+  retroPcsImage,
+  topLaptopsImage,
+  web3DesktopImage,
+  web3MobileImage,
+};
