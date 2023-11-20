514cd9c9ef30464e968967cbe7b961d343464599
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-20 19:45:20 +0530

    component 2-4

--- src/components/2-junior/index.js
+++ src/components/2-junior/index.js
@@ -1,5 +1,6 @@
 import NewsLetterSignUp from "./1-newsletter-sign-up/NewsLetterSignUp";
 import AgeCalculatorApp from "./2-age-calculator-app/AgeCalculatorApp";
 import NewsHomePage from "./3-news-home-page/NewsHomePage";
+import NotificationsPage from "./4-notifications-page/NotificationsPage";
 
-export { NewsLetterSignUp, AgeCalculatorApp, NewsHomePage };
+export { NewsLetterSignUp, AgeCalculatorApp, NewsHomePage, NotificationsPage };
