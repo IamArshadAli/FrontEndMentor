1a26bf43c5e6b39447c93bbb218ba9b54d368ab6
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-20 19:44:31 +0530

    assets 2-4

---
+++ src/assets/images/2-junior/4-notifications-page/index.js
@@ -0,0 +1,19 @@
+import angelaGrayAvatar from "./avatar-angela-gray.webp";
+import annaKimAvatar from "./avatar-anna-kim.webp";
+import jacobThompsonAvatar from "./avatar-jacob-thompson.webp";
+import kimberlySmithAvatar from "./avatar-kimberly-smith.webp";
+import markWebberAvatar from "./avatar-mark-webber.webp";
+import nathanPetersonAvatar from "./avatar-nathan-peterson.webp";
+import rizkyHasnuddinAvatar from "./avatar-rizky-hasanuddin.webp";
+import chessImage from "./image-chess.webp";
+
+export {
+  angelaGrayAvatar,
+  annaKimAvatar,
+  jacobThompsonAvatar,
+  kimberlySmithAvatar,
+  markWebberAvatar,
+  nathanPetersonAvatar,
+  rizkyHasnuddinAvatar,
+  chessImage,
+};
